3044624af018fb54ae9035e67251ea77a2596b27
Todd Baur <todd@toadkicker.com>
2017-08-12 13:29:35 +0900

    wtf example sucks

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,10 @@
-Braintree::Configuration.environment = ENV["BT_ENVIRONMENT"].to_sym
+# if ENV["BT_ENVIRONMENT"].nil?
+#   require 'logger'
+#   logger.info "The Braintree environment variable is not set. Payment processing unavailable."
+#   return
+# end
+
+Braintree::Configuration.environment = ENV["BT_ENVIRONMENT"].to_sym unless ENV['BT_ENVIRONMENT'].nil?
 Braintree::Configuration.merchant_id = ENV["BT_MERCHANT_ID"]
-Braintree::Configuration.public_key  = ENV["BT_PUBLIC_KEY"]
+Braintree::Configuration.public_key = ENV["BT_PUBLIC_KEY"]
 Braintree::Configuration.private_key = ENV["BT_PRIVATE_KEY"]
