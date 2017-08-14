9d240f51b95db2f4d747846a1387867258038997
Todd Baur <todd@toadkicker.com>
2017-08-14 13:49:28 +0900

    fix nil lang header because of scan call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
   private
 
   def read_lang_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{5}/).first unless request.env['HTTP_ACCEPT_LANGUAGE'].nil?
+    request.env['HTTP_ACCEPT_LANGUAGE'] unless request.env['HTTP_ACCEPT_LANGUAGE'].nil?
   end
 
   def flash_to_headers
