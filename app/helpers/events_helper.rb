48626f2a7a0d1c4aee21b52305c0caff244161f5
Todd Baur <todd@toadkicker.com>
2017-08-28 13:41:54 +0900

    make the event status on seat count consistent

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,23 @@
 module EventsHelper
+  def event_status_tag(max_seats)
+    if max_seats == -1
+      return content_tag :b, t('open_seating').upcase, class: 'badge badge-info'
+    end
+
+    if max_seats >= 1
+      return content_tag :b, [max_seats.to_s, t('available').upcase].join(' '), class: 'badge badge-info'
+    end
+
+    if max_seats == 0
+      return content_tag :b, t('sold_out').upcase, class: 'badge badge-danger'
+    end
+  end
+
+  def event_price_tag(price)
+    if price == 0
+      content_tag :b, t('free'), class: 'badge badge-danger'
+    else
+      humanized_money_with_symbol price
+    end
+  end
 end
