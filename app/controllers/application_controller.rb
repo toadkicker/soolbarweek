5520607f1da241e88820f71b3ce42787e66f2181
Todd Baur <todd@toadkicker.com>
2017-08-31 16:43:56 +0900

    change per https://github.com/plataformatec/devise/pull/4033/files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   layout "layout"
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
