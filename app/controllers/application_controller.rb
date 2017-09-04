4903028fa703c5df9c1a5ffa080d8018feb89fc8
Todd Baur <todd@toadkicker.com>
2017-09-04 17:51:36 +0900

    reset session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   layout "layout"
-  protect_from_forgery prepend: true
+  protect_from_forgery prepend: true, with: :reset_session
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
